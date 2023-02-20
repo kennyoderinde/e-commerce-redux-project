2de17ac5164dbfa02671881d58096e859987943e
kenny oderinde <kennyoderyndeh@gmail.com>
2023-02-20 20:25:48 +0100

    I added an input button to recieve values from users to print prime numbers

--- src/pages/MultiplicationTable.js
+++ src/pages/MultiplicationTable.js
@@ -1,73 +1,65 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./IsPrime.css"
-// import "./IsPrime.test"
 
+// function to check if a number is prime
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+// function to generate the first num prime numbers
+function generatePrimes(numPrimes) {
+  let primes = [];
+  let i = 2;
+  while (primes.length < numPrimes) {
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+    i++;
+  }
+  return primes;
+}
 
-  function isPrime(num) {
-    // function to check if a number is prime
-    for(let i = 2; i < num; i++) {
-      if(num % i === 0) {
-        return false;
-      }
-    }
-    return num !== 1;
-  }
-  
-  function getPrimes(num) {
-    // function to generate the first num prime numbers
-    const primes = [];
-    let i = 2;
-    while(primes.length < num) {
-      if(isPrime(i)) {
-        primes.push(i);
-      }
-      i++;
-    }
-    return primes;
-  }
-  
-  function generateTable(primes) {
-    // function to generate the multiplication table
-    const table = [];
-    for(let i = 0; i < primes.length; i++) {
-      const row = [];
-      for(let j = 0; j < primes.length; j++) {
-        row.push(primes[i] * primes[j]);
-      }
-      table.push(row);
-    }
-    return table;
-  }
-
+// function to generate the multiplication table
 function MultiplicationTable() {
-    const primes = getPrimes(10);
-    const table = generateTable(primes);
-  
-    return (
-    <div className="container">
-      <h1>Prime Numbers Multiplication Table</h1>
-      <table className="table">
-        <thead className="thead">
+  const [numPrimes, setNumPrimes] = useState(10);
+  const [primes, setPrimes] = useState(generatePrimes(numPrimes));
+
+  function handleNumPrimesChange(event) {
+    const newNumPrimes = parseInt(event.target.value);
+    setNumPrimes(newNumPrimes);
+    setPrimes(generatePrimes(newNumPrimes));
+  }
+
+  return (
+    <div className='container'>
+      <h1>Prime Numbers Multiplication Table</h1> 
+      <label className='head-label'>
+        Number of Primes:
+        <input className='input-box' type="number" value={numPrimes} onChange={handleNumPrimesChange} />
+      </label> <br></br>
+      <table className='table'>
+        <thead className='thead'>
           <tr>
             <th></th>
-            {primes.map((prime) => (
-              <th key={prime}>{prime}</th>
-            ))}
+            {primes.map(prime => <th key={prime}>{prime}</th>)}
           </tr>
         </thead>
-        <tbody className="table-body">
-          {primes.map((prime, i) => (
-            <tr key={prime}>
-              <th>{prime}</th>
-              {table[i].map((product, j) => (
-                <td key={j}>{product}</td>
+        <tbody className='table-body'>
+          {primes.map(prime1 => (
+            <tr key={prime1}>
+              <th>{prime1}</th>
+              {primes.map(prime2 => (
+                <td key={prime2}>{prime1 * prime2}</td>
               ))}
             </tr>
           ))}
         </tbody>
       </table>
-      </div>
-    );
-  }
-  
-  export default MultiplicationTable;
+    </div>
+  );
+}
+
+export default MultiplicationTable;
